b42514d481f7c708da3e98e2e5e753cbfc28528a
bigbrosci <lqlhz@outlook.com>
2026-01-14 16:20:59 -0500

    2025_01_14_v2

--- actions/center_atoms.py
+++ actions/center_atoms.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3 
 # center the clusters or gas phase species in the box 
 
+import sys
 from ase.io import read, write
 
-# Read the POSCAR file containing a cluster in a box
-cluster = read('POSCAR', format='vasp')
+def wrap_atoms(filename):
+    # Read the structure from the file
+    atoms = read(filename)
 
-# Center the cluster in the box
-#cluster.center(about=(0.5, 0.5, 0.5))
-cluster.center()
+    # Center the atoms in the unit cell
+    #atoms.center(about=(0.5, 0.5, 0.5))
+    atoms.center()
 
-# Wrap atoms into the unit cell
-cluster.wrap()
+    # Wrap the atoms using the pbc (periodic boundary conditions) and cell
+    atoms.wrap(pbc=True)
 
-# Save the centered structure as 'POSCAR_centered'
-write('POSCAR_centered', cluster, format='vasp', vasp5=True)
+    filename_out = filename + '_centered'
+    # Save the modified structure back to the file
+    write(filename_out, atoms, format='vasp', vasp5=True)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 wrap_atoms.py POSCAR")
+    else:
+        wrap_atoms(sys.argv[1])
+
